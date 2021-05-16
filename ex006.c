f7d44c32176c26349a2a3aa605a6166bb1f4c35a
Angell <angellbelger@hotmail.com>
2021-05-16 13:29:19 -0300

    fineshed ex006

--- ex006.c
+++ ex006.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
     int x, x_double, x_triple;
+    float x_sqr;
 
-    printf("Hello, world.\n");
-    printf("Type a number: ");
-    scanf("%i", x);
-    printf("Number: %i");
+    printf( "Hello, world.\n" );
+    printf( "Type a number: ");
+    scanf("%i", &x);
+    printf( "Number: %i" , x);
+
+    x_double = x * 2;
+    printf( "\nDouble: %i", x_double);
+
+    x_triple = x * 3;
+    printf( "\nTriple: %i" , x_triple);
+
+    x_sqr = sqrt(x);
+    printf( "\nSquare: %f", x_sqr);
     
     return x;
-
 }
