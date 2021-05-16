dc8009a9fac016fa132de23d7b4ff07743c7cdf5
Angell <angellbelger@hotmail.com>
2021-05-16 13:01:46 -0300

    up

--- ex006.c
+++ ex006.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-    double x, x_double, x_triple;
+    int x, x_double, x_triple;
     x_double = x * 2;
     x_triple = x * 3;
 
     printf("Hello, world.\n");
     printf("Type a number: ");
-    scanf("%f", x);
-    printf("Number: %f\nDouble: %f\nTriple: %f", x, x_double, x_triple);
+    scanf("%i", x);
+    printf("Number: %i\nDouble: %i\nTriple: %i", x, x_double, x_triple);
     
     return x;
 
