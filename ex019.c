2a43e50f4492cdfe11b3e8e7239974942940f386
Angell <angellbelger@hotmail.com>
2021-05-25 17:14:57 -0300

    update ex019

--- ex019.c
+++ ex019.c
@@ -2,7 +2,17 @@
 
 int main()
 {
-    printf("Hello, world.");
+    printf("\nHello, world.\n");
+
+    int x;
+
+    printf("\nType a number between 0 and 9999: ");
+    scanf("%i", &x);
+
+    printf("Unidade: %i\n", x % 10);
+    printf("Dezena: %i\n", x % 100);
+    printf("Centena: %i\n", x % 1000);
+    printf("Milhar: %i\n", x % 10000);
 
     return 0;
 }
