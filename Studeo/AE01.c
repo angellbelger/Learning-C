da33e3b6e1e64f23ec2871037e01d1e5a5d71849
Angell <angellbelger@hotmail.com>
2021-05-30 18:02:28 -0300

    Update AE01.c

--- Studeo/AE01.c
+++ Studeo/AE01.c
@@ -2,7 +2,15 @@
 
 int main()
 {
+    short int boolean = 0;
+    
     printf("\nHello, world.\n");
+
+    while ( boolean != 0 )
+    {
+        printf("------- Votation -------")
+    }
+    
 
     return 0;
 }
