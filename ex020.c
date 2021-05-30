aff3de92a2a5a0ee509f67f7bb7ca010562ed3bb
Angell <angellbelger@hotmail.com>
2021-05-30 14:46:06 -0300

    fineshed ex020

--- ex020.c
+++ ex020.c
@@ -4,5 +4,17 @@ int main()
 {
     printf("\nHello, world.\n");
 
+    int x;
+
+    printf("\nType a number: ");
+    scanf("%i", &x);
+
+    if ( x % 2 == 0)
+    {
+        printf("The number %i is Even.\n", x);
+    }else{
+        printf("The number %i is Odd.\n", x);
+    }
+
     return 0;
 }
