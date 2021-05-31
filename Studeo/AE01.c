53fac58d5cd4dd02681216afe4d275ef70f75fe3
Angell <angellbelger@hotmail.com>
2021-05-30 21:09:54 -0300

    update AE01

--- Studeo/AE01.c
+++ Studeo/AE01.c
@@ -2,15 +2,68 @@
 
 int main()
 {
-    short int boolean = 0;
+    int option, vote;
+    float vote_lula, vote_bolso, vote_amoedo;
     
     printf("\nHello, world.\n");
 
-    while ( boolean != 0 )
+    do
     {
-        printf("------- Votation -------")
-    }
-    
+
+        printf("\n\n---------- Voting ----------\n\n");
+        printf("\n1 - Vote\n2 - Polling\n3 - Exit");
+        printf("\nOption: ");
+        scanf("%i", &option);
+
+        switch (option)
+        {
+        case 1:
+            printf("\nTo President\n");
+            printf("\n13 - Lula\n17 - Bolsonaro\n30 - Amoedo\n");
+            printf("\nVote: ");
+            scanf("%i", &vote);
+            
+            switch (vote)
+            {
+            case 13:
+                vote_lula++;
+                break;
+            
+            case 17:
+                vote_bolso++;
+                break;
+            
+            case 30:
+                vote_amoedo++;
+                break;
+
+            default:
+            printf("\nInvalid Vote.\n");
+                break;
+            }
+        
+        case 2:
+            /*printf("\n---------- Polling ----------\n");
+            printf("\nResult:\n");
+            printf("\nLula: %.2f%%", ( (vote_lula / (vote_amoedo + vote_bolso + vote_lula)) * 100 ));
+            printf("\nBolsonaro: %.2f%%", ( (vote_bolso / (vote_amoedo + vote_bolso + vote_lula)) * 100 ));
+            printf("\nAmoedo: %.2f%%", ( (vote_amoedo / (vote_amoedo + vote_bolso + vote_lula)) * 100 ));*/
+            printf("%d", &vote_lula);
+            printf("%d", &vote_bolso);
+            printf("%d", &vote_amoedo);
+
+            break;
+        
+        case 3:
+            printf("\nok 3");
+            break;
+
+        default:
+            printf("\nInvalid Command/");
+            break;
+        }
+
+    } while ( option != 3 );
 
     return 0;
 }
