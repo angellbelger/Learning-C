3b61550e6f89b11b26773e65741f4c7e9880ae5e
Angell <angellbelger@hotmail.com>
2021-06-02 15:52:08 -0300

    update AE01

--- Studeo/AE01.c
+++ Studeo/AE01.c
@@ -2,9 +2,10 @@
 
 int main()
 {
-    int option, vote;
-    float vote_lula, vote_bolso, vote_amoedo;
-    vote_amoedo = vote_bolso = vote_lula = 0;
+    int option, vote, vote2;
+    option = 0;
+    float vote_joao, vote_maria, vote_ze, vote_zureta, vote_gomes;
+    vote_joao = vote_maria = vote_ze = vote_zureta = vote_gomes = 0;
     
     printf("\nHello, world.\n");
 
@@ -18,46 +19,78 @@ int main()
 
         switch (option)
         {
-        case 1:
-            printf("\nTo President\n");
-            printf("\n13 - Lula\n17 - Bolsonaro\n30 - Amoedo\n");
-            printf("\nVote: ");
-            scanf("%i", &vote);
+            case 1:
+                printf("\nTo city councilor.\n");
+                printf("\n111 - Joao do Frete\n222 - Maria da Pamonha\n333 - Ze da Farmacia\n444 - Voto nulo");
+                printf("\nVote: ");
+                scanf("%i", &vote);
+                
+                switch (vote)
+                {
+                    case 111:
+                        printf("\ncomputed vote.");
+                        vote_joao++;
+                        break;
+                    
+                    case 222:
+                        printf("\ncomputed vote.");
+                        vote_maria++;
+                        break;
+                    
+                    case 333:
+                    printf("\ncomputed vote.");
+                        vote_ze++;
+                        break;
+
+                    case 444:
+                        printf("\nVote void.");
+
+                    default:
+                        printf("\nInvalid Vote.\n");
+                        printf("\nVote void.");
+                        break;
+                }
+
+                printf("\nTo Mayor.\n");
+                printf("\n11 - Dr. Zureta\n22 - Sr. Gomes\n44 - Voto nulo");
+                printf("\nVote: ");
+                scanf("%i", &vote2);
+
+                switch (vote2)
+                {
+                    case 11:
+                        vote_zureta++;
+                        break;
+                    case 22:
+                        vote_gomes++;
+                        break;
+                    case 44:
+                        printf("\nInvalid Vote.\n");
+                        printf("\nVote void.");
+                        break;
+                    default:
+                        printf("\nInvalid Vote.\n");
+                        printf("\nVote void.");
+                        break;
+            }
             
-            switch (vote)
-            {
-            case 13:
-                vote_lula++;
+            case 2:
+                printf("\n---------- Polling ----------\n");
+                printf("\nResult:\n");
+                printf("\nJoao do Frete: %.2f%%", ( ( vote_joao / ( vote_joao + vote_maria + vote_ze )) * 100 ));
+                printf("\nMaria da Pamonha: %.2f%%", ( ( vote_maria / ( vote_joao + vote_maria + vote_ze )) * 100 ));
+                printf("\nZe da Farmacia: %.2f%%", ( ( vote_ze / ( vote_joao + vote_maria + vote_ze )) * 100 ));
+                printf("\n\n-----------------------------\n\n");
+                printf("\nDr. Zureta: %.2f%%", ( ( vote_zureta / ( vote_zureta + vote_gomes )) * 100 ));
+                printf("\nSr. Gomes: %.2f%%", ( (vote_gomes / ( vote_zureta + vote_gomes )) * 100 ));
                 break;
             
-            case 17:
-                vote_bolso++;
-                break;
-            
-            case 30:
-                vote_amoedo++;
+            case 3:
                 break;
 
             default:
-            printf("\nInvalid Vote.\n");
+                printf("\nInvalid Command/");
                 break;
-            }
-        
-        case 2:
-            printf("\n---------- Polling ----------\n");
-            printf("\nResult:\n");
-            printf("\nLula: %.2f%%", ( (vote_lula / (vote_amoedo + vote_bolso + vote_lula)) * 100 ));
-            printf("\nBolsonaro: %.2f%%", ( (vote_bolso / (vote_amoedo + vote_bolso + vote_lula)) * 100 ));
-            printf("\nAmoedo: %.2f%%", ( (vote_amoedo / (vote_amoedo + vote_bolso + vote_lula)) * 100 ));
-
-            break;
-        
-        case 3:
-            break;
-
-        default:
-            printf("\nInvalid Command/");
-            break;
         }
 
     } while ( option != 3 );
