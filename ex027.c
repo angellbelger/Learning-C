4a887f43804d90755ceb637800c9961998ae6749
Angell <angellbelger@hotmail.com>
2021-06-06 18:06:09 -0300

    cread ex027

---
+++ ex027.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\nHello, world.\n");
+
+    float a, b, c;
+
+    printf("\nSize of side A: ");
+    scanf("%f", &a);
+    printf("Size of side B: ");
+    scanf("%f", &b);
+    printf("Size of side C: ");
+    scanf("%f", &c);
+
+    while ( x == 1 )
+        if ( a < b + c && a > ( b - c) )
+            printf("Ok condition.\n");
+        if ( b < a + c && b > ( a - c ) )
+            printf("Ok condition.\n");
+        if ( c < a + b && c < ( a - b ) )
+            printf("Ok condition.\n");
+
+
+    return 0;
+}
