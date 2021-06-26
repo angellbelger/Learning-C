515d534f815c3e6951f2da8e69a39a32bfc4d667
Angell <angellbelger@hotmail.com>
2021-06-26 17:52:26 -0300

    ex043 fineshed

--- ex043.c
+++ ex043.c
@@ -2,13 +2,14 @@
 
 int main()
 {
-    printf("Hello, world.");
-
-    int x;
-
-    x = 27 | 17;
-
-    printf("%i\n", x);
+    int v1, v2, r;
+    printf("Type the first value: ");
+    scanf("%i", &v1);
+    printf("Type the second value: ");
+    scanf("%i", &v2);
+    printf("\n%i & %i = %i", v1, v2, v1 & v2);
+    printf("\n%i | %i = %i", v1, v2, v1 | v2);
+    printf("\n%i ^ %i = %i\n", v1, v2, v1 ^ v2);
 
     return 0;
 }
