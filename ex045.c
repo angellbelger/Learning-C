89009e6f0ad59845c7354b8082695abffebf194e
Angell <angellbelger@hotmail.com>
2021-06-27 14:27:20 -0300

    ex045 fineshed

--- ex045.c
+++ ex045.c
@@ -2,5 +2,20 @@
 
 int main()
 {
-    
+    int vectorA[10];
+    int i;
+
+    for (i=0; i<10; i++)
+    {
+        printf("Type a %i number: ", i);
+        scanf("%i", &vectorA[i]);
+
+    }
+    for (i=0; i<10; i++)
+    {
+        printf("%i ", vectorA[i]);
+    }
+    return 0;
+
+
 }
