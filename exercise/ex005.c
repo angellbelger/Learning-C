ec959840c2f04bb7e3d586ab06a73d1086d7fe5c
Angell Belger <angellbelger@hotmail.com>
2022-05-09 16:19:13 -0300

    fineshed ex005

--- exercise/ex005.c
+++ exercise/ex005.c
@@ -2,10 +2,15 @@
 #include <time.h>
 
 int main(){
+    srand(time(NULL));
 
-    srand
+    int myNumber, numberPc;
+
+    numberPc = rand() % 5 + 1;
 
     printf("\nhello, world.\n");
 
-    printf("I am thinking a number, can you guess what it is? ");
+    printf("I am thinking a number beetween 1 and 5, can you guess what it is? ");
+    scanf("%i", &myNumber);
+    printf("My number is %i, your number is %i.\n", numberPc, myNumber);
 }
