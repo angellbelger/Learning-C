b43023f0eef4c25ef76af29877cba750cd2ba9ae
Angell Belger <angellbelger@hotmail.com>
2022-05-09 14:32:28 -0300

    fineshed ex002

---
+++ exercise/ex002.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    printf("\nExercicio especial\n");
+    printf("==========================\n");
+    printf("\n\\a\t = \tBeep\n");
+    printf("\\n\t = \tBreak row\n");
+    printf("\\t\t = \tTab\n");
+    printf("\\\t = \tBar\n");
+    printf("%%\t = \tPercentage\n");
+    printf("\?\t = \tQuestion mark\n");
+    printf("==========================\n");
+
+    return 0;
+}
