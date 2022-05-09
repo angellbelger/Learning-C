b8818c219f36ef999d9b75764d9cd190b3df7e54
Angell Belger <angellbelger@hotmail.com>
2022-05-09 15:21:40 -0300

    update

--- ex034.c
+++ ex034.c
@@ -11,31 +11,29 @@ int main()
 
     printf("\n\n----------------- List -----------------\n\n");
     printf("First people: ");
-    scanf("%s", name1);
+    scanf("%s", &name1);
     fflush(stdin);
-    printf("Sex: ");
+    printf("\nSex: ");
     scanf("%c", &sex1);
     fflush(stdin);
-    printf("Note: ");
+    printf("\nNote: ");
     scanf("%f", &note1);
     fflush(stdin);
-    printf("Second people: ");
-    scanf("%s", name2);
+    printf("\nSecond people: ");
+    scanf("%s", &name2);
     fflush(stdin);
-    printf("Sex: ");
+    printf("\nSex: ");
     scanf("%c", &sex2);
     fflush(stdin);
-    printf("Note: ");
+    printf("\nNote: ");
     scanf("%f", &note2);
     fflush(stdin);
     printf("\n\n----------------- Learners -----------------\n\n");
-    printf("%-10s %-5s %-10s", "Name", "Sex","Note");
-    printf("\n%-10s %-5c %-10.2f", name1, sex1, note1);
-    printf("\n%-10s %-5c %-10.2f", name2, sex2, note2);
+    printf("%-10s %-5s %-10s", "Name", "Sex","Note\n");
+    printf("%-10s %-5c %-10.2f", name1, sex1, note1);
+    printf("%-10s %-5c %-10.2f", name2, sex2, note2);
     printf("\n");
     printf("\n--------------------------------------------\n\n");
 
-
-
-    
+    return 0;    
 }
