30750f29b7d9390f20b6e56b28cbae89f6a5b6d4
Angell Belger <angellbelger@hotmail.com>
2022-05-21 15:39:06 -0300

    ex059 fineshed

---
+++ ex059.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("\nHello, world.\n");
+
+    int len, i;
+    int *vector;
+
+    printf("\nTamanho do vetor: ");
+    scanf("%i", &len);
+    
+    vector = (int *) malloc(sizeof(int) * len);
+    for (i = 0; i < len; i++){
+        vector[i] = pow(2, i);
+        printf("%i\n", vector[i]);
+    }
+}
