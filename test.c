2755851937b4f31d7acb9c72413c7deb989c7391
Angell Belger <angellbelger@hotmail.com>
2022-06-18 16:40:44 -0300

    update

--- test.c
+++ test.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
-int main(void) {
-   int i = 1;
-   while (i <= 10){
-      ++i; 
-   }
+typedef struct Curso{
+   int id;
+   char nomeCurso[100];
+   int duracao;
+   char nomeCoordenador;
+}Curso;
+
+typedef struct aluno{
+   int ra;
+   char aluno[100];
+   int dia, mes, ano;
+   struct Curso curso;
+}aluno;
+
+int main(void){
+   printf("\nHello, world.\n");
+
+   return 0;
 }
