91bced3153d17709f5118bdaae79ce42155fa414
Angell Belger <angellbelger@hotmail.com>
2022-06-20 14:43:12 -0300

    up

--- test.c
+++ test.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-int main() {
-   int tam;
-   int *vetor;
-   printf ("Escolha o tamanho do vetor: ");
-   scanf("%d", &tam);
-   vetor = (int *) malloc(sizeof (int)*tam);
-   for (int i = 0; i < tam; i++) {
-      vetor[i] = pow(2,i);
-      printf ("Posicao %d: %d\n", i, vetor[i]);
-   }
-   system("Pause");
-}
+
+char nomeAluno[] = "jubileu";
+
+struct matricula{
+      char *nome = "jaja";
+};
