cb53cea87023b9eba22e5d1155666d8dff6d4d8b
Angell Belger <angellbelger@hotmail.com>
2022-07-05 14:29:19 -0300

    Update test.c

--- test.c
+++ test.c
@@ -1,12 +1,17 @@
-#include <stdio.h>  
-int main(int argc, char **argv) {  
-    int vetor[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};  
-    int i = 0, temp;  
-    while (i < 10){  
-        temp = vetor[i];  
-        vetor[i] = vetor[9-i];  
-        vetor[9-i] = temp;  
-        printf("%d ",vetor[i]);  
-        i++;              
-    }  
-} 
+int menor, maior, meio;
+    menor = 0;
+    maior = tam-1;
+    while (menor <= maior){
+       meio = (menor + maior)/2;
+       if (arg == vec[meio]){
+            return(meio);
+        }
+        if(arg < vec[meio]){
+            maior = meio - 1;
+        }
+       else {
+          menor = meio + 1;
+       }
+    }
+    return(-1);
+}
